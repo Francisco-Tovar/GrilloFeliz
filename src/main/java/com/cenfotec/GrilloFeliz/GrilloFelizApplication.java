e3e2c4196a4336750a2a03e25af65fe6ad318a11
Francisco-Tovar <55120226+Francisco-Tovar@users.noreply.github.com>
2022-05-01 12:26:38 -0600

    initializer creation

---
+++ src/main/java/com/cenfotec/GrilloFeliz/GrilloFelizApplication.java
@@ -0,0 +1,13 @@
+package com.cenfotec.GrilloFeliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrilloFelizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrilloFelizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cenfotec/GrilloFeliz/GrilloFelizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.GrilloFeliz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrilloFelizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
